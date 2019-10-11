1cac93f4fb1db09376b030287d173b4350a2da90
NaveenKumar <catch.nkn@gmail.com>
2019-10-11 15:19:06 -0400

    Updated the commands

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,5 +1,4 @@
 Write-Host "Execution Started in non gui mode with the following arguments: $args"
 $command = -join(".\jmeter ","$args")
 Invoke-Expression $command
-Invoke-Expression dir
-Write-Host "Execution has been completed."
+Write-Host "Execution has been completed. Please check your host machine for the results."
