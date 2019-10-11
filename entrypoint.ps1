c1235c3dfd981f28d838645b765da136c9d66ea7
NaveenKumar <catch.nkn@gmail.com>
2019-10-10 20:43:44 -0400

    dir cmd

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,4 +1,5 @@
 Write-Host "Execution Started in non gui mode with the following arguments: $args"
 $command = -join(".\jmeter ","$args")
 Invoke-Expression $command
+Invoke-Expression dir
 Write-Host "Execution has been completed."
